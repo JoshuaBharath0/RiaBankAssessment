4c84bd1ed699407a0af9728009f624d15566a824
JoshuaBharath <joshuabharath0@gmail.com>
2026-04-14 00:39:17 +0200

    TestcaseAndReadmeFileUpdate

--- src/main/java/com/bank/bankaccount/entities/BankAccountEntity.java
+++ src/main/java/com/bank/bankaccount/entities/BankAccountEntity.java
@@ -2,8 +2,11 @@ package com.bank.bankaccount.entities;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Entity
 @Data
@@ -14,14 +17,19 @@ public class BankAccountEntity {
 
     @NotBlank(message = "account holder cannot be empty")
     private String accountHolder;
-
-    @Column(unique = true)
+    //Optimize DB storage for IBANs
+    @Column(unique = true,length = 34)
     @NotBlank(message = "Account number is required")
+    @Pattern(
+            regexp = "^[A-Z0-9]{7,34}$",
+            message = "Invalid format: Use 7-12 digits for Domestic (SA) or up to 34 UPPERCASE alphanumeric characters for International (IBAN)."
+    )
     private String accountNumber;
 
     // we use this annotation as you never register with a bank with a negative bank balance
+    //made it BigDecimal instead as money could be lost using double during calculations
     @PositiveOrZero(message = "balance must be greater or equal 0")
-    private Double balance;
+    private BigDecimal balance;
 
 
 
